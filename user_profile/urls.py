7d8bde010f4d97daba1a5bdbe0b14f1e8b28eeba
Zeynep <zeynepakbalikk@gmail.com>
2023-10-08 22:49:24 +0300

    created login page

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -4,6 +4,5 @@ from .views import login_view
 # ın different app can be same url to avoid confusion
 app_name = 'user_profile'
 urlpatterns = [
-    path('login/', login_view, name='login_view')
-    ,
+    path('login/', login_view, name='login_view'),
 ]
--- user_profile/views.py
+++ user_profile/views.py
@@ -1,8 +1,22 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+from django.shortcuts import render, redirect
 
 
 def login_view(request):
-    # if user authenticated direct to  home page
-    if request.user.is_authenticated:
-        return render(request, 'base.html')
-    return render(request, 'login.html')
+    context = {
+
+    }
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        print(f"username: {email} password: {password}")
+        user = authenticate(request, username=email, password=password)
+        # TODO: info user abt logged in
+        # TODO: validate fields are correct or not
+        if user is not None:
+
+            login(request, user)
+            # you can directly redirect to view
+            return redirect('home_view')
+    return render(request, 'login.html', context)
