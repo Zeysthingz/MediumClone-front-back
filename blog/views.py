c7118c4ab3f640611e3c33fba8a13942d990abf4
Zeynep <zeynepakbalikk@gmail.com>
2023-10-31 01:00:02 +0300

    added django extensions

--- blog/views.py
+++ blog/views.py
@@ -7,16 +7,17 @@ from .forms import BlogModelForm
 @login_required(login_url='user_profile:login_view')
 def create_blog_view(request):
     form = BlogModelForm()
-
+    context = {
+        'form': form,
+    }
     if request.method == 'POST':
         # to get media files we should add request.FILES or None
         form = BlogModelForm(request.POST or None, request.FILES or None)
         if form.is_valid():
             f = form.save(commit=False)
             f.user = request.user
+            print(form.cleaned_data)
+            print(form.cleaned_data.get('tag'))
             # f.save()
-        context = {
-            'form': form,
-        }
 
     return render(request, 'create_blog.html', context)
