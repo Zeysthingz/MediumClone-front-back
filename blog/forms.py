200d13d1447f2c797a45e4b61a7dc1c9e8058e1c
Zeynep <zeynepakbalikk@gmail.com>
2023-10-30 22:21:57 +0300

    used form validators to validate title field

--- blog/forms.py
+++ blog/forms.py
@@ -1,9 +1,11 @@
 from django import forms
 from .models import Blog
+from tinymce.widgets import TinyMCE
 
 
 class BlogModelForm(forms.ModelForm):
     tag = forms.CharField(max_length=200, required=False)
+    content = forms.CharField(widget=TinyMCE(attrs={'cols': 20, 'rows': 10}))
 
     class Meta:
         model = Blog
@@ -14,6 +16,12 @@ class BlogModelForm(forms.ModelForm):
             'category',
             'tag',
         ]
+
+    def clean_title(self):
+        title = self.cleaned_data.get('title')
+        if len(title) < 2:
+            raise forms.ValidationError('Please enter valid title more than 2 characters')
+        return title
 
     # automatically initialized widget
     # def __init__(self, *args, **kwargs):
