e1264bb221d04909dbd78957ce615c0e29c120c8
Zeynep <zeynepakbalikk@gmail.com>
2023-10-31 01:49:57 +0300

    workin on stmhing shitty

--- blog/models.py
+++ blog/models.py
@@ -35,8 +35,6 @@ class BlogCategory(BaseModel):
 
 
 class BlogTag(BaseModel):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-
     def __str__(self):
         return self.title
 
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+import json
 from .models import Blog, BlogCategory, BlogTag
 from .forms import BlogModelForm
 
@@ -16,8 +17,14 @@ def create_blog_view(request):
         if form.is_valid():
             f = form.save(commit=False)
             f.user = request.user
-            print(form.cleaned_data)
-            print(form.cleaned_data.get('tag'))
-            # f.save()
+            f.save()
+            tags_data = json.loads(form.cleaned_data.get('tag'))
+            print(type(tags_data))
+            for tag_data in tags_data:
+                print(tag_data)
+
+                # tag_items, created = BlogTag.objects.get_or_create(title=item.item.get('value'))
+                # # save to form
+                # f.tag.add(tag_items)
 
     return render(request, 'create_blog.html', context)
